d494bf54bd17a3a91ab26bcb88bed46d980a26c5
typovrak <typovrak@gmail.com>
2025-05-24 20:16:58 +0200

    starting header custom module css

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,19 @@
+import Link from "next/link";
 import headerCss from "@/styles/components/header.module.css";
+import IconStar from "@/icons/Star";
 
 export default function Header() {
 	return (
 		<header className={headerCss.header}>
-			test
+			<div className={headerCss.wrapper}>
+				<Link href="/" className={headerCss.logo}>
+					<div>
+						<IconStar/>
+					</div>
+
+					<strong>Star Rune</strong>
+				</Link>
+			</div>
 		</header>
 	)
 }
