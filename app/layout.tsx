76109d4a1225ecbf0e4a6d8003eb305cd5d716c2
typovrak <typovrak@gmail.com>
2025-05-28 21:07:33 +0200

    commit before changing nvim config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import '@/styles/globals.css'
+import fonts from 'next/font/google'
 
 export const metadata: Metadata = {
   title: 'Star Rune landing page',
@@ -13,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={}>{children}</body>
     </html>
   )
 }
--- components/section-header.tsx
+++ components/section-header.tsx
@@ -3,12 +3,11 @@ import Badge from "@/components/badge";
 export default function SectionHeader({ badge, title, text }: { badge: { icon: ReactElement, title: string | ReactNode }, title: string, text: string }) {
 	return (
 		<div className="flex flex-col items-center justify-center space-y-4 text-center">
-		{/* TODO: title clipping */}
 			<Badge icon={badge.icon} title={badge.title} />
 
 			<div className="space-y-2 max-w-3xl">
-				<h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-amber-600">{title}</h2>
-				<p className="text-muted-foreground md:text-xl/relaxed lg:text-xl/relaxed max-w-[85%] mx-auto">{text}</p>
+				<h2 className="leading-[1.25] sm:leading-[1.25] md:leading-[1.25] lg:leading-[1.25] text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-amber-600">{title}</h2>
+				<p className="text-muted-foreground md:text-xl lg:text-xl max-w-[900px] mx-auto">{text}</p>
 			</div>
 		</div>
 	)
