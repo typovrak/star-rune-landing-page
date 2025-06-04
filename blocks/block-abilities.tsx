cd4d6e07ce3f1b13aef077768f1eb88c9858151d
typovrak <typovrak@gmail.com>
2025-06-04 23:46:46 +0200

    init slides variable for <BlockAbilities>

--- blocks/block-abilities.tsx
+++ blocks/block-abilities.tsx
@@ -3,8 +3,54 @@ import SectionWrapper from "@/layouts/section-wrapper";
 import { Gamepad2 } from "lucide-react";
 import data from "@/utils/data";
 import GameplaySection from "@/components/gameplay-section";
+import Shield from "@/icons/shield";
+import Sparkles from "@/icons/sparkles";
+import Bolt from "@/icons/bolt";
+import Fall from "@/icons/fall";
+import Rocket from "@/icons/rocket";
+import Swift from "@/icons/swift";
 
 export default function BlockAbilities({ id }: IBlockAbilities) {
+
+	const slides = [
+		{
+			icon: <Shield />,
+			iconHover: <Shield className="" />,
+			title: "Auto-deflect",
+			text: "Nova automatically will deflect Azki lasers. Later, you can upgrade your sword(s) to reflect these at Glitchborn to deal extra damage.",
+		},
+		{
+			icon: <Swift />,
+			iconHover: <Swift className="" />,
+			title: "Dash",
+			text: "When you see a highlighted Azki, type that letter, you'll dash through them and build up your Charge.",
+		},
+		{
+			icon: <Bolt />,
+			iconHover: <Bolt className="" />,
+			title: "Chain dash",
+			text: "When you see consecutive Azki of the same type, hold their key down and you'll quickly slash through all of them.",
+		},
+		{
+			icon: <Fall />,
+			iconHover: <Fall />,
+			title: "Fast fall + Meteor slash",
+			text: "Press [TAB] to perform a Fast Fall. Pressing it while moving downward creates a Meteor Slash.",
+		},
+		{
+			icon: <Rocket />,
+			iconHover: <Rocket className="" />,
+			title: "Shift power",
+			text: "Hold [SHIFT] or activate [CAPS] to use Shift Power. This will make your dash line thicker and stronger.",
+		},
+		{
+			icon: <Sparkles />,
+			iconHover: <Sparkles className="" />,
+			title: "Full charge",
+			text: "When you have Full Charge, your sword glows a new color. Dash lines are stronger and last longer.",
+		},
+	];
+
 	return (
 		<SectionWrapper
 			id={id}
---
+++ icons/sparkles.tsx
@@ -0,0 +1,7 @@
+import { ISVG } from "@/utils/types";
+
+export default function Sparkles({ className }: ISVG) {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M16 18a2 2 0 0 1 2 2a2 2 0 0 1 2 -2a2 2 0 0 1 -2 -2a2 2 0 0 1 -2 2zm0 -12a2 2 0 0 1 2 2a2 2 0 0 1 2 -2a2 2 0 0 1 -2 -2a2 2 0 0 1 -2 2zm-7 12a6 6 0 0 1 6 -6a6 6 0 0 1 -6 -6a6 6 0 0 1 -6 6a6 6 0 0 1 6 6z" /></svg>
+	)
+}
